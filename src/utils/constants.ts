d93ea5b6b34bebb7d7deebf9dcc09788e70e7167
origiladi <120328585+origiladi@users.noreply.github.com>
2024-04-08 23:17:00 +0300

    using the deployed server adress

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const EMAIL_JS_PUBLIC_KEY = import.meta.env.VITE_EMAIL_JS_PUBLIC_KEY
 export const EMAIL_JS_TEMPLATE_ID = import.meta.env.VITE_EMAIL_JS_TEMPLATE_ID
 export const EMAIL_JS_SERVICE_ID = import.meta.env.VITE_EMAIL_JS_SERVICE_ID
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = import.meta.env.VITE_BASE_URL
 export const NOT_FOUND_STATUS_CODE = 404;
 export const UNAUTHORIZED_STATUS_CODE = 401;
 export const BAD_REQUEST_STATUS_CODE = 400;
